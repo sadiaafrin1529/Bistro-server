e5885bb4d74795dc5cdb3ef112abdb5111fb214c
unknown <sadiaafrin1529@gmail.com>
2025-01-30 09:01:48 +0600

    userDelete

--- index.js
+++ index.js
@@ -25,35 +25,65 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("bistroDB").collection("users");
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewsCollection = client.db("bistroDB").collection("reviews");
     const cartsCollection = client.db("bistroDB").collection("carts");
 
 
-    app.get("/carts", async (req, res) => {
-      const email = req.query.email;
-      const query={email:email}
-      const result = await cartsCollection.find(query).toArray();
+
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      //insert email if user doesn't exist
+      //we can do this many way(1.email unique 2.upsert 3.simple checking)
+      //3
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query)
+      if (existingUser) {
+      return res.send({ message: " User Already exists", insertedId: null });
+      }
+      const result = await usersCollection.insertOne(user);
       res.send(result)
 })
 
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+
     
     
+    
+    
+    
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartsCollection.insertOne(cartItem);
-       res.send(result)
-    })
+      res.send(result);
+    });
 
-const { ObjectId } = require("mongodb");
- app.delete("/carts/:id", async (req, res) => {
-   const id = req.params.id;
-   const query = { _id: new ObjectId(id) };
-   const result = await cartsCollection.deleteOne(query);
-   res.send(result);
- });
-
-
+    const { ObjectId } = require("mongodb");
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    });
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
