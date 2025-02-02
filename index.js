b6c7d3151d6c24f7d7273c788562207a5c704a8f
unknown <sadiaafrin1529@gmail.com>
2025-02-03 02:16:06 +0600

    role added

--- index.js
+++ index.js
@@ -52,6 +52,20 @@ async function run() {
       res.send(result)
 })
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      //id ta k admin korbo
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: "admin"
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+    
+    
     app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
